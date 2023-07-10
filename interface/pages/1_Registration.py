bba97bad1fa4ce735dc838f413ebc867fcbf218e
gio <giomikecs17@gmail.com>
2023-07-10 00:01:04 -0500

    DermDetect interface v1.0

--- interface/pages/1_Registration.py
+++ interface/pages/1_Registration.py
@@ -6,7 +6,7 @@ from utils.nav import *
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 
-
+import os
 # --- USER AUTHENTICATION ---
 names = ["Peter Parker", "Rebecca Miller"]
 usernames = ["pparker", "rmiller"]
@@ -16,7 +16,8 @@ hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../security/config.yaml') as file:
+path_temporal = os.path.dirname(os.path.abspath(__file__)) 
+with open(os.path.join(path_temporal, '..','/security/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 for username, hashed_password in zip(config['credentials']['usernames'].keys(), hashed_passwords):
     config['credentials']['usernames'][username]['password'] = hashed_password
